837e39f4dd5e3eff97d4ed64fbbbe9058719744c
Matt Kesler <mkesler@captechconsulting.com>
2017-01-10 10:43:15 -0500

    Test spaces

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Roulette from './components/Roulette.js';
 import './App.css';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="container">
-				<h1>
-					<picture>
-						<source srcSet={logoMobile} media="(max-width: 600px)"/>
-						<source srcSet={logoDesktop} media="(min-width: 601px)"/>
-						<img src={logoDesktop} alt="Karaoke Roulette"/>
-					</picture>
-				</h1>
-				<Roulette />
-			</div>
-		);
-	}
+   render() {
+      return (
+         <div className="container">
+            <h1>
+               <picture>
+                  <source srcSet={logoMobile} media="(max-width: 600px)"/>
+                  <source srcSet={logoDesktop} media="(min-width: 601px)"/>
+                  <img src={logoDesktop} alt="Karaoke Roulette"/>
+               </picture>
+            </h1>
+            <Roulette />
+         </div>
+      );
+   }
 }
 
 export default App;
